57ec7caf1e7d860bc43ea761eef28c6b3e246b2b
Hxrmn1 <Hxrmn1@5e9ee0ac-6352-0410-8b85-cd337fa8ff94>
2008-12-10 21:49:01 +0000

    -Created instagib game

---
+++ trunk/code/debug/lua/cl_instagib.lua
@@ -0,0 +1,107 @@
+local shd = LoadShader("railCore")
+local clicksound = LoadSound("sound/weapons/noammo.wav")
+STAT_SHOTS = 1
+STAT_HITS = 2
+STAT_ACCURACY = 3
+STAT_DEATHS = 4
+local stats = {
+	[STAT_SHOTS] = {0,"Shots"},
+	[STAT_HITS] = {0,"Hits"},
+	[STAT_ACCURACY] = {0,"Accuracy",true},
+	[STAT_DEATHS] = {0,"Deaths"},
+}
+
+local function shouldDraw(str)
+	if(str == "HUD_STATUSBAR_HEALTH") then return false end
+	if(str == "HUD_STATUSBAR_AMMO") then return false end
+	if(str == "HUD_AMMOWARNING") then return false end
+	if(str == "HUD_WEAPONSELECT") then return false end
+end
+hook.add("ShouldDraw","cl_instagib",shouldDraw)
+
+local railStart = 0
+local railEnd = 0
+local t = 0
+local fading = false
+local function drawHud()
+	stats[STAT_ACCURACY][1] = math.floor((stats[STAT_HITS][1] / stats[STAT_SHOTS][1]) * 100)
+	if(stats[STAT_SHOTS][1] <= 0) then
+		stats[STAT_ACCURACY][1] = 0
+	end
+	draw.SetColor(1,1,1,.8)
+	local y = 0
+	for i=1, #stats do
+		local v = stats[i]
+		local txt = v[2] .. ": " .. v[1]
+		if(v[3]) then txt = txt .. "%" end
+		draw.Text(640 - (string.len(txt) * 15),y,txt,15,15)
+		y = y + 20
+	end
+end
+
+local function draw2d()
+	drawHud()
+	t = t + 1
+	local d = (railEnd - railStart)
+	local dt = (railEnd - LevelTime()) / d
+	local w = 100
+	local h = 8
+	local n = w * (1 - dt)
+	local pc = math.floor(10*(1-dt))*10 .. "%"
+	if(dt <= 0) then pc = "100%" end
+	local txt = "Reloading " .. pc
+	local c = 150 * (1-dt)
+
+	if(n > w) then n = w end
+	if(c > 150) then c = 150 end
+	
+	if(railStart != 0 and railEnd != 0) then
+		local al = 1
+		if(dt < 0) then al = (1 + (dt*3.5)) end
+		if(al < 0) then al = 0 end
+		draw.SetColor(1,1,1,.1*al)
+		draw.Rect(320 - w/2,260 - h/2,w,h)
+		draw.Rect(320 - w/2,260 - h/3,w,h*.3)
+
+		draw.SetColor(hsv(c,1,1,.4*al))
+		draw.Rect(320 - n/2,260 - h/2,n,h)
+		
+		draw.SetColor(hsv(c,.5,1,.4*al))
+		draw.Rect(320 - n/2,260 - h/3,n,h*.3)
+		
+		local x = 320 - (h*.8 * string.len(txt))/2
+		local y = 270 - h/2
+		draw.SetColor(0,0,0,.8*al)
+		draw.Text(x-1,y,txt,h*.8,h)
+		draw.Text(x+1,y,txt,h*.8,h)
+		draw.Text(x,y-1,txt,h*.8,h)
+		draw.Text(x,y+1,txt,h*.8,h)
+		draw.SetColor(1,1,1,.8*al)
+		draw.Text(x,y,txt,h*.8,h)
+		
+		if(dt <= 0 and fading == false) then
+			PlaySound(clicksound)
+			fading = true
+		end
+		if(al <= 0) then
+			railStart = 0
+			railEnd = 0
+		end
+	end
+end
+hook.add("Draw2D","cl_instagib",draw2d)
+
+local function HandleMessage(msgid)
+	if(msgid == "igrailfire") then
+		fading = false
+		if(railStart != 0 and railEnd != 0) then
+			PlaySound(clicksound)
+		end
+		railStart = message.ReadLong()
+		railEnd = message.ReadLong()
+	end
+	if(msgid == "igstat") then
+		stats[message.ReadShort()][1] = message.ReadShort()
+	end
+end
+hook.add("HandleMessage","cl_instagib",HandleMessage)
--- trunk/code/debug/lua/instagib.lua
+++ trunk/code/debug/lua/instagib.lua
@@ -1,3 +1,9 @@
+IG_RELOADTIME = 2000
+
+STAT_SHOTS = 1
+STAT_HITS = 2
+STAT_DEATHS = 4
+
 local let = {
 	[MOD_UNKNOWN] = 1,
 	[MOD_WATER] = 1,
@@ -8,48 +14,161 @@ local let = {
 	[MOD_SUICIDE] = 1,
 	[MOD_TARGET_LASER] = 1,
 	[MOD_TRIGGER_HURT] = 1,
+	--Hazards respawn the player
 }
 
-local function removeWeapons()
-	for k,v in pairs(GetAllEntities()) do
-		if(string.find(v:Classname(),"weapon_")) then
-			v:Remove()
+message.Precache("igrailfire")
+message.Precache("igstat")
+
+local function pickspawn()
+	local tab = GetEntitiesByClass("info_player_start")
+	table.Add(tab,GetEntitiesByClass("info_player_deathmatch"))
+	local point = tab[math.random(1,#tab)]
+	if(point != nil) then
+		return point
+	end
+	return nil
+end
+
+local function fullHealth(self) 
+	--Simple function set's player's health to full and set's a timer to do so after damage
+	self:SetHealth(100)
+	Timer(.001,self.SetHealth,self,100)
+end
+
+local function removePickups()
+	for k,v in pairs(GetAllEntities()) do --Loop through all the entities
+		if(string.find(v:Classname(),"weapon")) then
+			v:Remove() --If an entity's name contains 'weapon', remove it
+		end
+		if(string.find(v:Classname(),"ammo")) then
+			v:Remove() --If an entity's name contains 'ammo', remove it
+		end
+		if(string.find(v:Classname(),"armor")) then
+			v:Remove() --If an entity's name contains 'armor', then remove it
+		end
+		if(string.find(v:Classname(),"health")) then
+			v:Remove() --If an entity's name contains 'health', then remove it
+		end
+		if(string.find(v:Classname(),"item")) then
+			v:Remove() --If an entity's name contains 'item', then remove it
 		end
 	end
+end
+
+local function sendStat(pl,s)
+	local msg = Message()
+	message.WriteShort(msg,s)
+	message.WriteShort(msg,pl:GetTable().stats[s])
+	SendDataMessage(msg,pl,"igstat")
+end
+
+local function setStat(pl,s,i)
+	pl:GetTable().stats = pl:GetTable().stats or {}
+	pl:GetTable().stats[s] = i
+	sendStat(pl,s)
+end
+
+local function addStat(pl,s,i)
+	pl:GetTable().stats = pl:GetTable().stats or {}
+	pl:GetTable().stats[s] = pl:GetTable().stats[s] or 0
+	pl:GetTable().stats[s] = pl:GetTable().stats[s] + i
+	sendStat(pl,s)
 end
 
 local function setupPlayer(pl)
+	fullHealth(pl)
 	pl:RemoveWeapons() --Remove all of the player's weapons
-	pl:GiveWeapon(WP_RAILGUN) --Give the player a railgun
-	pl:SetAmmo(WP_RAILGUN,-1) -- -1 will make the ammo numbers go away :)
-	pl:SetWeapon(WP_RAILGUN) --Set the railgun as the active weapon
+	local function go()
+		pl:GiveWeapon(WP_RAILGUN) --Give the player a railgun
+		pl:SetAmmo(WP_RAILGUN,-1) -- -1 will make the ammo numbers go away :)
+		pl:SetWeapon(WP_RAILGUN) --Set the railgun as the active weapon
+	end
+	pl:SetSpeed(1.5)
 	if(pl:IsBot()) then pl:SetAmmo(WP_RAILGUN,999) end --Bots need full ammo or they won't shoot
+	pl:SetPowerup(PW_INVIS,3000)
+	pl:GetTable().gi_invistime = LevelTime() + 3000
+	Timer(2.5,go)
 end
-hook.add("PlayerSpawned","instagib",setupPlayer)
 
-local function PreDamage(self,inflictor,attacker,damage,dtype)
+local function spawnPlayer(self)
+	local spawn = pickspawn()
+	if(spawn) then
+		self:SetVelocity(Vector())
+		self:SetPos(spawn:GetPos())
+		CreateTempEntity(vAdd(self:GetPos(),Vector(0,0,10)),EV_PLAYER_TELEPORT_IN)
+		local angles = spawn:GetAngles()
+		if(angles) then
+			self:SetAngles(angles)
+		end
+		--setupPlayer(self)
+	end
+end
+
+local function PreDamage(self,inflictor,attacker,damage,dtype) 
+	--PreDamage is called BEFORE the player is damaged, and the returned value is the amount of damage the player will take
+	self:GetTable().gi_invistime = self:GetTable().gi_invistime or 0
 	if(attacker) then
 		if(attacker:GetInfo().weapon == WP_RAILGUN) then
+			if(self:GetTable().gi_invistime > LevelTime()) then
+				fullHealth(self)
+				return 0; --No damage to invisibles
+			end
 			--If the player was hit with a railgun
-			return 10000 --Just gib the player
+			addStat(attacker,STAT_HITS,1)
+			addStat(self,STAT_DEATHS,1)
+			return 200 --Just gib the player (loads of damage)
 		end
 	end
 	if(let[dtype] == nil) then
+		fullHealth(self)
 		return 0 --If we don't have an exception (aka hazard) then don't damage the player
+		--This makes it so that the player doesn't take falling damage
+	else
+		--Respawn the player when he touches a hazard
+		spawnPlayer(self)
+		fullHealth(self)
+		return 0
 	end
 end
-hook.add("PrePlayerDamaged","instagib",PreDamage)
 
 local function FiredWeapon(player,weapon,delay,pos,angle)
-	if(weapon == WP_RAILGUN) then
-		return delay/2 --Twice the fire rate
+	if(weapon == WP_RAILGUN and player != nil) then
+		local rt = IG_RELOADTIME
+		local maxv = 800
+		local rv = maxv - VectorLength(player:GetVelocity())
+		if(rv < 0) then rv = 0 end
+		rv = rv + 800
+		--rv = rv / 4
+		--if(VectorLength(player:GetVelocity()) > 200) then rt = rt / 2 end
+		local msg = Message()
+		message.WriteLong(msg,LevelTime())
+		message.WriteLong(msg,LevelTime() + rv)
+		SendDataMessage(msg,player,"igrailfire")
+		addStat(player,STAT_SHOTS,1)
+		return rv --fire rate
 	end
 end
+hook.add("PlayerSpawned","instagib",setupPlayer)
+hook.add("PrePlayerDamaged","instagib",PreDamage)
 hook.add("FiredWeapon","instagib",FiredWeapon)
 hook.add("ShouldDropItem","instagib",function() return false end) --Don't drop items (like railguns)
-
---Remove weapon pickups and outfit players
-removeWeapons()
+hook.add("PlayerKilled","instagib",function(p)  end)
+--Timer(1.5,spawnPlayer,p)
+--Remove pickups and outfit players
+removePickups()
 for k,v in pairs(GetAllPlayers()) do
 	setupPlayer(v)
+	setStat(v,STAT_SHOTS,0)
+	setStat(v,STAT_HITS,0)
 end
+
+SendScript("lua/cl_instagib.lua")
+
+
+local function reloadTime(p,c,a)
+	if(a[1] == nil) then return end
+	local n = tonumber(a[1])
+	if(n != nil) then IG_RELOADTIME = n end
+end
+concommand.Add("ReloadTime",reloadTime,true)
