011ddd290be66cee16ea1f2898a16c27b201edd0
Hxrmn1 <Hxrmn1@5e9ee0ac-6352-0410-8b85-cd337fa8ff94>
2009-01-13 20:18:26 +0000

    -It's a flying game!

---
+++ code/debug/lua/games/flying/init.lua
@@ -0,0 +1,15 @@
+function GAME:Think()
+	for k,v in pairs(GetAllPlayers()) do
+		if(v:IsPlayer() and v:GetHealth() > 0) then
+			--v:SetPowerup(PW_FLIGHT,POWERUP_FOREVER)
+			local bits = v:GetInfo()["buttons"];
+			print(bits .. "\n")
+			if(bitAnd(bits,16) != 0) then 
+			
+			end
+			
+			local f,r,u = AngleVectors(v:GetAimAngles())
+			--v:SetVelocity(f * 500)
+		end
+	end
+end
