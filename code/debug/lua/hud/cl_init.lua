ddb337c236cd584aaa1f9e7bd01e976e812bc708
Hxrmn1 <Hxrmn1@5e9ee0ac-6352-0410-8b85-cd337fa8ff94>
2009-02-13 06:25:03 +0000

    -Cleanup

--- code/debug/lua/hud/cl_init.lua
+++
@@ -1,4 +0,0 @@
-includesimple("hud/cl_hudutils");
-includesimple("hud/cl_hudpickups");
-includesimple("hud/cl_head");
-includesimple("hud/cl_hud");
--- code/debug/lua/hud/init.lua
+++
@@ -1,4 +0,0 @@
-SendScript("lua/hud/cl_hudutils.lua");
-SendScript("lua/hud/cl_hudpickups.lua");
-SendScript("lua/hud/cl_head.lua");
-SendScript("lua/hud/cl_hud.lua");
