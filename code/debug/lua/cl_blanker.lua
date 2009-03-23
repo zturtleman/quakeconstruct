e9477aa5bbdc673c6c126d9e31531724dc29e3ac
Hxrmn1 <Hxrmn1@5e9ee0ac-6352-0410-8b85-cd337fa8ff94>
2009-03-23 20:35:16 +0000

    -cl_blanker.lua does paint :)

--- code/debug/lua/cl_blanker.lua
+++ code/debug/lua/cl_blanker.lua
@@ -1,8 +1,49 @@
 --local shader = LoadShader("flareShader")
+local mx,my = 0,0
+local lmx,lmy = 0,0
+local lx,ly = 0,0
+local cx,cy = 0,0
+local function drawStuff()
+	if(MouseDown()) then
+		draw.SetColor(1,1,1,1)
+		draw.Line(cx,cy,lx,ly,nil,2)
+	end
+	--ly = ly + (my - ly)*.1
+	--lx = lx + (mx - lx)*.1
+	lx,ly = cx,cy
+	cx = cx + (mx - cx)*.1
+	cy = cy + (my - cy)*.1
+end
+
+local function moused(x,y)
+	mx = mx + x
+	my = my + y
+	
+	if(mx > 640) then mx = 640 end
+	if(mx < 0) then mx = 0 end
+	
+	if(my > 480) then my = 480 end
+	if(my < 0) then my = 0 end
+end
+hook.add("MouseEvent","cl_blanker",moused)
+
 local wmodel = LoadModel("*0")
+local clx = 0
 local function d3d()
 	draw.SetColor(0,0,0,1)
-	draw.Rect(0,0,640,480)
+	
+	if(KeyIsDown(K_SPACE)) then
+		draw.Rect(0,0,640,480)
+	end
+	
+	draw.Rect(lmx-1,lmy-1,2,2)
+	
+	draw.SetColor(1,1,1,1)
+	draw.Rect(mx-1,my-1,2,2)
+	
+	lmx,lmy = mx,my
+	
+	drawStuff()
 	
 	local ref = RefEntity()
 	ref:SetModel(wmodel)
@@ -19,8 +60,8 @@ local function UserCmd(pl,angle,fm,rm,um,buttons,weapon)
 end
 hook.add("UserCommand","cl_blanker",UserCmd)
 
-local function noWorld(str)
-	if(str == "WORLD") then return true end
+local function noNuthin(str)
+	--if(str == "WORLD") then return false end
 	return false
 end
-hook.add("ShouldDraw","cl_blanker",noWorld);
+hook.add("ShouldDraw","cl_blanker",noNuthin);
